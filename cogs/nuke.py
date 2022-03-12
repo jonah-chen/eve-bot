beb0de0ffbe636b840d033cfd6d5efb6b70e3768
jonah-chen <jonah.chen@mail.utoronto.ca>
2022-03-12 03:42:31 -0500

    unlocked nuke feature

--- cogs/nuke.py
+++ cogs/nuke.py
@@ -11,17 +11,12 @@ class Nuke(commands.Cog):
     @commands.command()
     async def nuke(self, ctx, *, passcode):
         passcode = passcode.lower()
-        if str(ctx.author) == "Chubbyman#3362":
-            if "genocidal" in passcode and "organ" in passcode:
-                self.nuke = True
-            elif "lucia" in passcode and "sukrova" in passcode:
-                self.nuke = False
-            else:
-                await ctx.send("Incorrect passcode.")
-
+        if "genocidal" in passcode and "organ" in passcode:
+            self.nuke = True
+        elif "lucia" in passcode and "sukrova" in passcode:
+            self.nuke = False
         else:
-            await ctx.send("Sorry, you are not permitted to use this command.")
-
+            await ctx.send("Incorrect passcode.")
         while self.nuke:
             await ctx.send("Death to @everyone")
 
